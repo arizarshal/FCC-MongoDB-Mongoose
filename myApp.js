49f383778a48e92d04415f0c54dcbf30130a5bfe
Ariz Arshal <arizarshal@gmail.com>
2023-03-24 20:56:21 +0530

    4th commit

--- myApp.js
+++ myApp.js
@@ -1,11 +1,22 @@
-require('dotenv').config();
+const dotenv =require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true },
   console.log('database connected successfully')
   );
 
-let Person;
+// let Person;
+
+const personSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+
+
+const Person = mongoose.model("Person", personSchema);
+
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
